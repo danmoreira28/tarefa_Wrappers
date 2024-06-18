5880c495b9e281376f2227e96ec0e74c91329b8a
Danilo Moreira <danilo_moreirasilva@hotmail.com>
2024-06-18 19:53:26 -0300

    Commit Initial

---
+++ src/danilo/moreira/Tarefamod9.java
@@ -0,0 +1,19 @@
+package danilo.moreira;
+
+public class Tarefamod9 {
+	
+	public static void main(String[] args) {
+	
+		int num = 25;
+		
+		System.out.println(num);
+		
+		Integer num2 = (int) num;
+		
+		System.out.println(num2);
+		
+
+		
+	}
+}
+
